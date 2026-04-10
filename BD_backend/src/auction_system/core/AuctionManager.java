ab31b4b010fb57f03d564d65f72b6b1f082a115e
TruongThanhDat <truongthanhdat019@gmail.com>
2026-04-11 00:07:17 +0700

    add core folder and AuctionManager in core

---
+++ BD_backend/src/auction_system/core/AuctionManager.java
@@ -0,0 +1,40 @@
+package auction_system.core;
+
+import auction_system.model.Auction;
+
+import java.util.*;
+
+//Singleton
+public class AuctionManager {
+    private static volatile AuctionManager instance = null;
+    private Map<String, Auction> auctions;
+
+    private AuctionManager() {
+        auctions = new HashMap<>();
+    }
+
+    public static AuctionManager getInstance() {
+        if (instance == null) { //lazy init
+            synchronized (AuctionManager.class) {
+                if (instance == null) {
+                    instance = new AuctionManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void createAuction() {}
+
+    public Auction getAuction(String id) {
+        return auctions.get(id);
+    }
+    public List<Auction> getAllAuctions() {
+        return (ArrayList) auctions.values();
+    }
+
+    public void removeAuction(String id) {
+        auctions.remove(id);
+    }
+
+}
