c41ac41edb4869b3903895a1363e58bb61e4138b
TruongThanhDat <truongthanhdat019@gmail.com>
2026-04-11 21:02:55 +0700

    thêm MainApplication.java là ctr chính

---
+++ src/main/java/client/MainApplication.java
@@ -0,0 +1,18 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
+public class MainApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
