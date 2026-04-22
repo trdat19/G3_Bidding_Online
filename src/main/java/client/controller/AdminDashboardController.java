55d34ccc4edaff6c1efbeaf43fb6384f594b532a
Vanhh06 <vietanh1234567hp@gmail.com>
2026-04-22 14:44:29 +0700

    save work

--- src/main/java/client/controller/AdminDashboardController.java
+++ src/main/java/client/controller/AdminDashboardController.java
@@ -1,5 +1,38 @@
 package client.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AdminDashboardController {
+    @FXML
+    private void handleLogout() {
 
+    }
+    @FXML
+    private void showDashboard() {
+
+    }
+    @FXML
+    private void showUsers() {
+
+    }
+    @FXML
+    private void showAuctions() {
+
+    }
+    @FXML
+    private void showProducts() {
+
+    }
+    @FXML
+    private void showReports() {
+
+    }
+    @FXML
+    private void showSettings() {
+
+    }
 }
