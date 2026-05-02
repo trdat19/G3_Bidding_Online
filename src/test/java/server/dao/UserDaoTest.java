820e5270711173518b580c51067ca8a3579a05a1
TruongThanhDat <truongthanhdat019@gmail.com>
2026-05-02 22:01:58 +0700

    chore: delete trash

--- src/test/java/server/dao/UserDaoTest.java
+++ src/test/java/server/dao/UserDaoTest.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class UserDaoTest {
+public class UserDAOTest {
     private Connection connection;
     private UserDao userDAO;
 
