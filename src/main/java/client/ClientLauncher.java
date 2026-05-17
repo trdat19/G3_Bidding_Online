9d80e1f5d9dca998c4aab5572f9fa1086b6c6970
TruongThanhDat <truongthanhdat019@gmail.com>
2026-05-18 00:49:04 +0700

    feat(pom): thêm maven-shade-plugins để executable jar chạy nhiều client

---
+++ src/main/java/client/ClientLauncher.java
@@ -0,0 +1,9 @@
+package client;
+
+public class ClientLauncher {
+
+    public static void main(String[] args) {
+        // Gọi tới class JavaFX chính của bạn
+        MainApplication.main(args);
+    }
+}
